99e21e38447d75b8b56541634b9345fb4fda9265
Alexander Massimino <49351994+alexmassimino9@users.noreply.github.com>
2024-02-13 16:02:29 -0500

    invoices page

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
--- src/views/index.js
+++ src/views/index.js
@@ -2,4 +2,5 @@ import { Sidemenu, Topbar } from "./global";
 import { Dashboard } from "./dashboard";
 import { Team } from "./team";
 import { Contacts } from "./contacts";
-export { Sidemenu, Topbar, Dashboard, Team, Contacts };
+import { Invoices } from "./invoices";
+export { Sidemenu, Topbar, Dashboard, Team, Contacts, Invoices };
